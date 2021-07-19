0e514368e1898c12be89dad34576545de822afab
fate <dhmony-99@hotmail.com>
2021-07-19 12:58:57 +0300

    Reuse venv for .nox

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ def gen_stubs(session: nox.Session) -> None:
 	session.install("-r", "requirements.txt", "-r", "dev-requirements.txt")
 	session.run('stubgen', *FILES, '-o', '.', '--include-private', '--no-import', external=True)
 
-@nox.session
+@nox.session(reuse_venv=True)
 def type_check(session: nox.Session) -> None:
 	gen_stubs(session)
 	session.install("-r", "requirements.txt", "-r", "dev-requirements.txt")

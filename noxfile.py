542297a216a72b6abe6221be62661fcd47f2a379
fate <dhmony-99@hotmail.com>
2021-07-19 10:38:46 +0300

    Maybe fixed?

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,11 @@ import nox
 FILES = ['aiobungie/__init__.py', 'aiobungie/objects/__init__.py', 'aiobungie/utils/__init__.py']
 
 def gen_stubs(session: nox.Session) -> None:
-	session.install('.[dev]')
-	session.run('stubgen', *FILES, '-o', '.', '--include-private', '--no-import')
+	session.install("-r", "requirements.txt", "-r", "dev-requirements.txt")
+	session.run('stubgen', *FILES, '-o', '.', '--include-private', '--no-import', external=True)
 
 @nox.session
-def type_check(session) -> None:
+def type_check(session: nox.Session) -> None:
 	gen_stubs(session)
-	session.install('.[dev]')
+	session.install("-r", "requirements.txt", "-r", "dev-requirements.txt")
 	session.run('mypy', 'aiobungie', external=True)

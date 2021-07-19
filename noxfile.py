03a0e4be719d385ad3644658946372c9477a6f13
fate <dhmony-99@hotmail.com>
2021-07-19 10:30:24 +0300

    install dev for mypy tests.

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,11 @@ import nox
 FILES = ['aiobungie/__init__.py', 'aiobungie/objects/__init__.py', 'aiobungie/utils/__init__.py']
 
 def gen_stubs(session: nox.Session) -> None:
-	session.install('.')
+	session.install('.[dev]')
 	session.run('stubgen', *FILES, '-o', '.', '--include-private', '--no-import')
 
 @nox.session
 def type_check(session) -> None:
 	gen_stubs(session)
-	session.install('.')
+	session.install('.[dev]')
 	session.run('mypy', 'aiobungie', external=True)

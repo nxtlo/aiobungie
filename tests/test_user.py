3f436cc1d6e8b08df15dd8f55aa17c97fc4a5d2d
fate <dhmony-99@hotmail.com>
2021-10-19 14:56:10 +0300

    Revert to typing objs.

--- tests/test_user.py
+++ tests/test_user.py
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from __future__ import annotations
 from aiobungie.internal import helpers
 from datetime import datetime
 

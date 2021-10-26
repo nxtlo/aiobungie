edab58a045b64db3e25829975fba036bffa04d11
fate <dhmony-99@hotmail.com>
2021-10-26 16:00:30 +0300

    Add React app example.

---
+++ examples/web_app/backend/rest.py
@@ -0,0 +1,51 @@
+"""A simple Python REST only API using the REST client.
+
+All objects returned are JSON objects which we can view them in the frontend.
+"""
+import typing
+
+import aiobungie
+from aiobungie.internal import helpers
+
+from backend import config
+
+# The rest client.
+rest = aiobungie.RESTClient(config.CLIENT_TOKEN)
+
+# A helpers function to close the rest client.
+async def close() -> None:
+    await rest.close()
+
+
+# Fetch some users from their id.
+async def fetch_user(id: int) -> helpers.JsonObject:
+    return await rest.fetch_user(id)
+
+
+# Fetch a clan.
+async def fetch_clan(name_or_id: str) -> helpers.JsonObject:
+    return await rest.fetch_clan(name_or_id)
+
+
+# Convert the user input membership type from string to an int.
+def _convert_membership(type: typing.Optional[str] = None) -> aiobungie.MembershipType:
+    convert: aiobungie.MembershipType
+    if type is None:
+        convert = aiobungie.MembershipType.ALL
+    if type == "Steam":
+        convert = aiobungie.MembershipType.STEAM
+    elif type == "Xbox":
+        convert = aiobungie.MembershipType.XBOX
+    elif type == "PSN":
+        convert = aiobungie.MembershipType.PSN
+    elif type == "Stadia":
+        convert = aiobungie.MembershipType.STADIA
+    else:
+        convert = aiobungie.MembershipType.ALL
+    return convert
+
+
+async def fetch_player(
+    name: str, member_type: typing.Optional[str] = None
+) -> helpers.JsonArray:
+    return await rest.fetch_player(name, _convert_membership(member_type))

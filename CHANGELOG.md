2aadc90cd3d4ecfa14a170acc82210116a6e8212
fate <dhmony-99@hotmail.com>
2022-02-26 03:27:46 +0300

    Prepare new release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).All notable changes to this project will be documented in this file.
 
-## [Unreleased](https://github.com/nxtlo/aiobungie/compare/0.2.5...HEAD)
+## [Unreleased](https://github.com/nxtlo/aiobungie/compare/0.2.6a0...HEAD)
 
+## [0.2.6a0](https://github.com/nxtlo/aiobungie/compare/0.2.5...0.2.6a0) 2022-02-26
 ## Added
 - `RESTClient` now takes an extra parameter `enable_debugging`, If set to `True` then
 it will debug responses and log them.
--- examples/transfer_items.py
+++ examples/transfer_items.py
@@ -1,92 +1,84 @@
 # -*- coding: utf-8 -*-
 
-"""An example on fetching a titan character, inventory and try to transfer an item to a hunter."""
+"""An example on how to transfer items from a character to another."""
 
 import aiobungie
 from aiobungie.internal import helpers
 
+client = aiobungie.Client("TOKEN")
 
-class ClientAware:
-    def __init__(self) -> None:
-        self.client = aiobungie.Client("TOKEN")
-        self.membership_type = aiobungie.MembershipType.STEAM
-        # Those are dummy ids, You should put yours here.
-        self.membership_id = 4401
-        self.titan_id = 2234
-        self.hunter_id = 2235
-
-    async def titan_worker(self) -> None:
-        """A method that fetch the titan character and transfer an item to the hunter."""
-        try:
-            character_request = await self.client.fetch_character(
-                self.membership_id,
-                self.membership_type,
-                self.titan_id,
-                # Character component
-                aiobungie.ComponentType.CHARACTERS,
-                # Character's inventory component.
-                aiobungie.ComponentType.CHARACTER_INVENTORY,
-            )
-
-            # Check if the titan character component is not None
-            if titan := character_request.character:
-                print(titan.emblem, titan.id, titan.class_type)
-
-                # Check if the titan inventory component is not None.
-                if inventory := character_request.inventory:
-                    for item in inventory:
-                        # Try to transfer an item.
-                        if (
-                            # Check if the item is One thousand Voices in our titan inventory.
-                            item.hash == 2069224589
-                            # Check if the item can be transferred.
-                            and item.is_transferable
-                        ):
-                            assert item.instance_id is not None
-                            # Try to transfer the item to hunter through the rest.
-                            try:
-                                # A bearer access token must be provided to be able to
-                                # make OAuth2 requests otherwise this will not work.
-
-                                # To ensure items are transferred to your character
-                                # you must first transfer it to vault then from vault to character.
-
-                                # You can either use `asyncio.create_task(...)` or
-                                # builtin `helpers.awaits(...)` function to transfer items concurrently
-                                # to ensure you don't get ratelimited assuming You're serving hundereds of requests a minute.
-
-                                await helpers.awaits(
-                                    self.client.rest.transfer_item(
-                                        "BEARER_TOKEN",
-                                        item.instance_id,
-                                        item.hash,
-                                        # From titan to vault.
-                                        titan.id,
-                                        titan.member_type,
-                                        vault=True,
-                                    ),
-                                    self.client.rest.transfer_item(
-                                        "BEARER_TOKEN",
-                                        item.instance_id,
-                                        item.hash,
-                                        # From vault to hunter.
-                                        self.hunter_id,
-                                        self.membership_type,
-                                    ),
-                                )
-                                print("Transferred item success!.")
-                            except aiobungie.Unauthorized as err:
-                                print("Can't tranfer item for unautorized users.", err)
-                                pass
-
-        except aiobungie.CharacterError:
-            print(f"Couldn't find the titan for player id {self.membership_id}.")
-            return
-
-    def run_it(self) -> None:
-        """A method to run our titan worker."""
-        self.client.run(self.titan_worker())
+HUNTER_ID = 4401
+TITAN_ID = 4402
+MEMBERSHIP_ID = 4403
+MEMBERSHIP_TYPE = aiobungie.MembershipType.STEAM
 
 
-client = ClientAware()
-client.run_it()
+async def fetch_my_titan() -> aiobungie.crate.CharacterComponent:
+    """A helper function to fetch our titan character and return both character and inventory components."""
+    return await client.fetch_character(
+        MEMBERSHIP_ID,
+        MEMBERSHIP_TYPE,
+        TITAN_ID,
+        # Titian character component
+        aiobungie.ComponentType.CHARACTERS,
+        # Titan's inventory component.
+        aiobungie.ComponentType.CHARACTER_INVENTORY,
+    )
+
+
+async def transfer() -> None:
+    """A helper function to transfer our items from a character to another."""
+
+    my_titan = await fetch_my_titan()
+
+    # Check if the inventory component is not empty.
+    if inventory := my_titan.inventory:
+        for item in inventory:
+            # Try to transfer an item.
+            if (
+                # Check if the item is One thousand Voices in our titan inventory.
+                item.hash == 2069224589
+                # Check if the item can be transferred.
+                and item.is_transferable
+            ):
+                assert item.instance_id is not None
+
+                # Transfer the item.
+                try:
+                    # ! Notes
+
+                    # * A bearer access token must be provided to be able to
+                    # * make OAuth2 requests otherwise this will not work.
+
+                    # * To ensure items are transferred to your character
+                    # * you must first transfer it to vault then from vault to character.
+
+                    # * You can either use `asyncio.create_task(...)` or
+                    # * builtin `helpers.awaits(...)` function to transfer items concurrently
+                    # * to ensure you don't get ratelimited assuming You're serving hundereds of requests a minute.
+
+                    await helpers.awaits(
+                        client.rest.transfer_item(
+                            "BEARER_TOKEN",
+                            item.instance_id,
+                            item.hash,
+                            # From titan to vault.
+                            TITAN_ID,
+                            MEMBERSHIP_TYPE,
+                            vault=True,
+                        ),
+                        client.rest.transfer_item(
+                            "BEARER_TOKEN",
+                            item.instance_id,
+                            item.hash,
+                            # From vault to hunter.
+                            HUNTER_ID,
+                            MEMBERSHIP_TYPE,
+                        ),
+                    )
+                    print("Transferred item success!.")
+
+                # Handle the error.
+                except aiobungie.HTTPError as err:
+                    print(f"Couldn't transfer item due to {err.message}.")
+                    return
